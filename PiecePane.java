69ef38bd5256c857b82364cdbf4b8434ed55f630
Christopher Rodriguez <ckrod16@gmail.com>
2022-01-21 10:45:46 -0500

    piecemovement

---
+++ PiecePane.java
@@ -0,0 +1,77 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+import javafx.scene.shape.Rectangle;
+
+public class PiecePane extends Pane {
+    public final double radius = 20;
+    private double x = radius, y = radius;
+    private double dx = 1, dy = 1;
+    private Rectangle r1 = new Rectangle(x, y, 50, 50);
+    private Timeline animation;
+
+    public PiecePane() {
+        r1.setFill(Color.GREEN); // Set ball color
+        getChildren().add(r1); // Place a ball into this pane
+        
+        // Create an animation for moving the ball
+        animation = new Timeline(
+            new KeyFrame(Duration.millis(50), e -> move()));
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play(); // Start animation
+    }
+
+    public void play() {
+        animation.play();
+    }
+
+    public void pause() {
+        animation.pause();
+    }
+
+    public void increaseSpeed() {
+        animation.setRate(animation.getRate() + 0.1);
+    }
+
+    public void decreaseSpeed() {
+        animation.setRate(
+            animation.getRate() > 0 ? animation.getRate() - 0.1 : 0);
+    }
+
+    public void left(){
+        dx = -1;
+    }
+
+    public void right(){
+        dx = 1;
+    }
+
+    public void stop(){
+        dx = 0;
+        dy = 0
+    }
+
+    public DoubleProperty rateProperty() {
+        return animation.rateProperty();
+    }
+
+    protected void move() {
+        // Check boundaries
+        // if (x < radius || x > getWidth() - radius) {
+        //     dx *= -1; // Change ball move direction
+        // }
+        if (y > getHeight() - 50) {
+             dy *= 0; // Change ball move direction
+         }
+        // Adjust ball position
+        x += dx;
+        y += dy;
+        dx = 0;
+        r1.setX(x);
+        r1.setY(y);
+    }
+}
---
+++ pieceControl.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+
+public class pieceControl extends Application {
+    public void start(Stage primaryStage){
+        PiecePane piecePane = new PiecePane();
+        // Pause and resume animation
+        piecePane.setOnMousePressed(e -> piecePane.pause());
+        piecePane.setOnMouseReleased(e -> piecePane.play());
+        // Increase and decrease animation
+        piecePane.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.UP) {
+                piecePane.increaseSpeed();
+            }
+            else if (e.getCode() == KeyCode.DOWN) {
+                piecePane.decreaseSpeed();
+            }
+            if(e.getCode() == KeyCode.RIGHT){
+                piecePane.right();
+            }else if(e.getCode() == KeyCode.LEFT){
+                piecePane.left();
+            }
+        });
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(piecePane, 250, 150);
+        primaryStage.setTitle("PieceMovement"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+        // Must request focus after the primary stage is displayed
+        piecePane.requestFocus();
+    }
+}
